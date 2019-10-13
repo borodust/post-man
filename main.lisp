ce82d294e250e1481af4c8ca6a605f112df947b2
Pavel Korolev <dev@borodust.org>
2019-10-13 23:34:40 +0300

    Add game definition

--- main.lisp
+++ main.lisp
@@ -1,5 +1,14 @@
 (cl:in-package :post-statics)
 
+(gamekit:defgame post-statics (gamekit.postproc:postproc)
+  ()
+  (:viewport-width 960)
+  (:viewport-height 540)
+  (:viewport-title "Post Statics")
+  (:canvas-width 270)
+  (:canvas-height 270)
+  (:default-initargs :postproc-indirect-width 480
+                     :postproc-indirect-height 270))
 
 (defun run ()
-  (print "Coming soon"))
+  (gamekit:start 'post-statics))
--- post-statics.asd
+++ post-statics.asd
@@ -3,7 +3,7 @@
   :description "Autumn Lisp Game Jam 2019 entry"
   :author ""
   :license "GPLv3"
-  :depends-on (:trivial-gamekit)
+  :depends-on (:trivial-gamekit :trivial-gamekit-postproc)
   :serial t
   :components ((:file "packages")
                (:file "main")))
