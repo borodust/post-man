38b020ddc45777de2cc5da07a27ffbf2a2fcb4f6
Pavel Korolev <dev@borodust.org>
2019-10-21 04:25:21 +0300

    Return testing build with SBCL

--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,6 @@
 (cl:in-package :post-man)
 
-(gamekit:defgame post-man (gamekit.postproc:postproc gamekit.fistmachine:fistmachine) ()
+(gamekit:defgame post-man (gamekit.fistmachine:fistmachine) ()
   (:viewport-width (* *grid-size* *grid-cell-width*))
   (:viewport-height (* *grid-size* *grid-cell-width*))
   (:viewport-title "POST-MAN")
